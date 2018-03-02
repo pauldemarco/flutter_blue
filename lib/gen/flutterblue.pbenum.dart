5ed788dbed0da70645a896a5662f5152c7240c1a
Paul DeMarco <paulmdemarco@gmail.com>
2018-03-02 16:22:07 -0500

    Updated protobuf dependency (fixes #33). Removed redundant dependencies in example's pubspec (fixes #3).

--- lib/gen/flutterblue.pbenum.dart
+++ lib/gen/flutterblue.pbenum.dart
@@ -1,11 +1,10 @@
 ///
 //  Generated code. Do not modify.
 ///
-// ignore_for_file: non_constant_identifier_names
-// ignore_for_file: library_prefixes
+// ignore_for_file: non_constant_identifier_names,library_prefixes
 library flutterblue_pbenum;
 
-// ignore: UNUSED_SHOWN_NAME
+// ignore_for_file: UNDEFINED_SHOWN_NAME,UNUSED_SHOWN_NAME
 import 'dart:core' show int, dynamic, String, List, Map;
 import 'package:protobuf/protobuf.dart';
 
