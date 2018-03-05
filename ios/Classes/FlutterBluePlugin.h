939c745ab21aa190eace948b4c9a0f9db7bbd4fd
KryptoAsh <->
2018-03-04 20:51:25 -0500

    Method and Event channels added

--- ios/Classes/FlutterBluePlugin.h
+++ ios/Classes/FlutterBluePlugin.h
@@ -1,4 +1,10 @@
 #import <Flutter/Flutter.h>
 
+#define NAMESPACE @"plugins.pauldemarco.com/flutter_blue"
+
 @interface FlutterBluePlugin : NSObject<FlutterPlugin>
 @end
+
+@interface StateStreamHandler : NSObject<FlutterStreamHandler>
+@end
+
--- ios/Classes/FlutterBluePlugin.m
+++ ios/Classes/FlutterBluePlugin.m
@@ -1,20 +1,64 @@
 #import "FlutterBluePlugin.h"
 
+@interface FlutterBluePlugin ()
+@property(nonatomic, retain) NSObject<FlutterPluginRegistrar> *registrar;
+@property(nonatomic, retain) FlutterMethodChannel *channel;
+@property(nonatomic, retain) FlutterEventChannel *stateChannel;
+@property(nonatomic, retain) FlutterEventChannel *scanResultChannel;
+@property(nonatomic, retain) FlutterEventChannel *servicesDiscoveredChannel;
+@property(nonatomic, retain) FlutterEventChannel *characteristicReadChannel;
+@property(nonatomic, retain) FlutterEventChannel *descriptorReadChannel;
+@property(nonatomic, retain) FlutterEventChannel *characteristicNotifiedChannel;
+
+@end
+
 @implementation FlutterBluePlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"flutter_blue"
-            binaryMessenger:[registrar messenger]];
-  FlutterBluePlugin* instance = [[FlutterBluePlugin alloc] init];
-  [registrar addMethodCallDelegate:instance channel:channel];
+    FlutterMethodChannel* channel = [FlutterMethodChannel
+                                     methodChannelWithName:NAMESPACE @"/methods"
+                                     binaryMessenger:[registrar messenger]];
+    FlutterEventChannel* stateChannel = [FlutterEventChannel eventChannelWithName:NAMESPACE @"/state" binaryMessenger:[registrar messenger]];
+    FlutterEventChannel* scanResultChannel = [FlutterEventChannel eventChannelWithName:NAMESPACE @"/scanResult" binaryMessenger:[registrar messenger]];
+    FlutterEventChannel* servicesDiscoveredChannel = [FlutterEventChannel eventChannelWithName:NAMESPACE @"/servicesDiscovered" binaryMessenger:[registrar messenger]];
+    FlutterEventChannel* characteristicReadChannel = [FlutterEventChannel eventChannelWithName:NAMESPACE @"/characteristicRead" binaryMessenger:[registrar messenger]];
+    FlutterEventChannel* descriptorReadChannel = [FlutterEventChannel eventChannelWithName:NAMESPACE @"/descriptorRead" binaryMessenger:[registrar messenger]];
+    FlutterEventChannel* characteristicNotifiedChannel = [FlutterEventChannel eventChannelWithName:NAMESPACE @"/characteristicNotified" binaryMessenger:[registrar messenger]];
+    FlutterBluePlugin* instance = [[FlutterBluePlugin alloc] init];
+    instance.channel = channel;
+    instance.stateChannel = stateChannel;
+    instance.scanResultChannel = scanResultChannel;
+    instance.servicesDiscoveredChannel = servicesDiscoveredChannel;
+    instance.characteristicReadChannel = characteristicReadChannel;
+    instance.descriptorReadChannel = descriptorReadChannel;
+    instance.characteristicNotifiedChannel = characteristicNotifiedChannel;
+    
+    StateStreamHandler* stateStreamHandler = [[StateStreamHandler alloc] init];
+    [stateChannel setStreamHandler:stateStreamHandler];
+    
+    [registrar addMethodCallDelegate:instance channel:channel];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
-    result(FlutterMethodNotImplemented);
-  }
+    if ([@"getPlatformVersion" isEqualToString:call.method]) {
+        result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+    } else {
+        result(FlutterMethodNotImplemented);
+    }
 }
 
 @end
+
+@implementation StateStreamHandler
+
+- (FlutterError*)onListenWithArguments:(id)arguments eventSink:(FlutterEventSink)eventSink {
+    
+    return nil;
+}
+
+- (FlutterError*)onCancelWithArguments:(id)arguments {
+    
+    return nil;
+}
+
+@end
+
