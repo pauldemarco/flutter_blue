a0f9f2f4002b3ed9fa29f1be0962f7b1992bd152
demarcoMac <->
2018-03-05 14:10:26 -0500

    FlutterBlue state and onStateChanged implemented

--- ios/Classes/FlutterBluePlugin.h
+++ ios/Classes/FlutterBluePlugin.h
@@ -1,10 +1,12 @@
 #import <Flutter/Flutter.h>
+#import <CoreBluetooth/CoreBluetooth.h>
 
 #define NAMESPACE @"plugins.pauldemarco.com/flutter_blue"
 
-@interface FlutterBluePlugin : NSObject<FlutterPlugin>
+@interface FlutterBluePlugin : NSObject<FlutterPlugin, CBCentralManagerDelegate>
 @end
 
 @interface StateStreamHandler : NSObject<FlutterStreamHandler>
+@property FlutterEventSink sink;
 @end
 
