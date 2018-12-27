b9d653916453a65540acd2eef2a495b0862ab649
Paul DeMarco <paulmdemarco@gmail.com>
2018-12-27 18:34:53 -0500

    Fixed possible NPE [Android].

--- android/src/main/java/com/pauldemarco/flutterblue/FlutterBluePlugin.java
+++ android/src/main/java/com/pauldemarco/flutterblue/FlutterBluePlugin.java
@@ -554,7 +554,7 @@ public class FlutterBluePlugin implements MethodCallHandler, RequestPermissionsR
             public void onReceive(Context context, Intent intent) {
                 final String action = intent.getAction();
 
-                if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
+                if (BluetoothAdapter.ACTION_STATE_CHANGED.equals(action)) {
                     final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE,
                             BluetoothAdapter.ERROR);
                     switch (state) {
