8c615f7f6f00225f99f301cfe7670608ad78cc43
Paul DeMarco <paulmdemarco@gmail.com>
2018-12-28 21:31:19 -0500

    isScanning, isDiscoveringServices, scanResults and more...
    startScan/stopScan offer a way to start and stop a scan without having to be subscribed for the results. Results will show up in the new `scanResults` stream.
    
    Introduced rxdart for more powerful reactive behavior:
    FlutterBlue.instance `state` is now a Stream.
    BluetoothDevice `services` is now a Stream.
    Moved read/write capability from BluetoothDevice into Characteristic/Descriptor.
    BluetoothCharacteristic/Descriptor now have value Streams.
    
    Removed separate event channels for scanResults, servicesDiscovered, characteristicReads, descriptorReads. They now use the main MethodChannel. #11

--- lib/src/bluetooth_descriptor.dart
+++ lib/src/bluetooth_descriptor.dart
@@ -9,15 +9,76 @@ class BluetoothDescriptor {
 
   final Guid uuid;
   final DeviceIdentifier deviceId;
-  final Guid serviceUuid; // The service that this descriptor belongs to.
-  final Guid
-      characteristicUuid; // The characteristic that this descriptor belongs to.
-  List<int> value;
+  final Guid serviceUuid;
+  final Guid characteristicUuid;
+
+  BehaviorSubject<List<int>> _value;
+  Stream<List<int>> get value => _value.stream;
+
+  List<int> get lastValue => _value.value;
 
   BluetoothDescriptor.fromProto(protos.BluetoothDescriptor p)
       : uuid = new Guid(p.uuid),
         deviceId = new DeviceIdentifier(p.remoteId),
         serviceUuid = new Guid(p.serviceUuid),
         characteristicUuid = new Guid(p.characteristicUuid),
-        value = p.value;
+        _value = BehaviorSubject(seedValue: p.value);
+
+  /// Retrieves the value of a specified descriptor
+  Future<List<int>> read() async {
+    var request = protos.ReadDescriptorRequest.create()
+      ..remoteId = deviceId.toString()
+      ..descriptorUuid = uuid.toString()
+      ..characteristicUuid = characteristicUuid.toString()
+      ..serviceUuid = serviceUuid.toString();
+
+    await FlutterBlue.instance._channel
+        .invokeMethod('readDescriptor', request.writeToBuffer());
+
+    return FlutterBlue.instance._methodStream
+        .where((m) => m.method == "ReadDescriptorResponse")
+        .map((m) => m.arguments)
+        .map((buffer) => new protos.ReadDescriptorResponse.fromBuffer(buffer))
+        .where((p) =>
+            (p.request.remoteId == request.remoteId) &&
+            (p.request.descriptorUuid == request.descriptorUuid) &&
+            (p.request.characteristicUuid == request.characteristicUuid) &&
+            (p.request.serviceUuid == request.serviceUuid))
+        .map((d) => d.value)
+        .first
+        .then((d) {
+      _value.add(d);
+      return d;
+    });
+  }
+
+  /// Writes the value of a descriptor
+  Future<Null> write(List<int> value) async {
+    var request = protos.WriteDescriptorRequest.create()
+      ..remoteId = deviceId.toString()
+      ..descriptorUuid = uuid.toString()
+      ..characteristicUuid = characteristicUuid.toString()
+      ..serviceUuid = serviceUuid.toString()
+      ..value = value;
+
+    await FlutterBlue.instance._channel
+        .invokeMethod('writeDescriptor', request.writeToBuffer());
+
+    return FlutterBlue.instance._methodStream
+        .where((m) => m.method == "WriteDescriptorResponse")
+        .map((m) => m.arguments)
+        .map((buffer) => new protos.WriteDescriptorResponse.fromBuffer(buffer))
+        .where((p) =>
+            (p.request.remoteId == request.remoteId) &&
+            (p.request.descriptorUuid == request.descriptorUuid) &&
+            (p.request.characteristicUuid == request.characteristicUuid) &&
+            (p.request.serviceUuid == request.serviceUuid))
+        .first
+        .then((w) => w.success)
+        .then((success) => (!success)
+            ? throw new Exception('Failed to write the descriptor')
+            : null)
+        .then((_) => _value.add(value))
+        .then((_) => null);
+  }
 }
