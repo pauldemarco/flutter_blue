525eac559095cc1dc45af4b6f5627044fb035498
Paul DeMarco <paulmdemarco@gmail.com>
2019-01-12 23:37:01 -0500

    Added missing copyright notices.

--- ios/Classes/FlutterBluePlugin.h
+++ ios/Classes/FlutterBluePlugin.h
@@ -1,3 +1,7 @@
+// Copyright 2017, Paul DeMarco.
+// All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 #import <Flutter/Flutter.h>
 #import <CoreBluetooth/CoreBluetooth.h>
 
