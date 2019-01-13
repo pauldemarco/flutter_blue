ec012a32f532ef9e8e4f8eeb0d21c58f5936bec8
Paul DeMarco <paulmdemarco@gmail.com>
2019-01-12 23:37:01 -0500

    Added missing copyright notices.

--- example/lib/widgets.dart
+++ example/lib/widgets.dart
@@ -1,3 +1,7 @@
+// Copyright 2017, Paul DeMarco.
+// All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 
