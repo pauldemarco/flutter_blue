2ccdc68d474c158ae7cfbcae993e430a578173ec
Paul DeMarco <paulmdemarco@gmail.com>
2019-10-18 03:17:29 -0400

    Update project files (#401)
    * Update project files for Android and iOS
    * Remove dependency on protoc in iOS podspec file

--- example/android/app/src/main/java/com/pauldemarco/flutter_blue_example/MainActivity.java
+++ example/android/app/src/main/java/com/pauldemarco/flutter_blue_example/MainActivity.java
@@ -1,7 +1,6 @@
-package com.pauldemarco.flutterblueexample;
+package com.pauldemarco.flutter_blue_example;
 
 import android.os.Bundle;
-
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
