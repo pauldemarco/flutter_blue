b4fe193fd5951b6d72f33425edd199c8c51ff59b
AsafVaron <asaf.varon@axonius.com>
2020-05-24 15:04:19 +0300

    convention

--- android/src/main/java/com/pauldemarco/flutter_blue/FlutterBluePlugin.java
+++ android/src/main/java/com/pauldemarco/flutter_blue/FlutterBluePlugin.java
@@ -1017,7 +1017,7 @@ public class FlutterBluePlugin implements FlutterPlugin, ActivityAware, MethodCa
 
     // BluetoothDeviceCache contains any other cached information not stored in Android Bluetooth API
     // but still needed Dart side.
-    class BluetoothDeviceCache {
+    static class BluetoothDeviceCache {
         final BluetoothGatt gatt;
         int mtu;
 
