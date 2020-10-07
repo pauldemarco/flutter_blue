9976cb7e93d30ab8d1b0536e92897f9646298a1b
Adam Burnett <adamburnett@Adams-MacBook-Pro.local>
2020-10-07 09:21:18 -0600

    added exception catch for scan results

--- android/src/main/java/com/pauldemarco/flutter_blue/FlutterBluePlugin.java
+++ android/src/main/java/com/pauldemarco/flutter_blue/FlutterBluePlugin.java
@@ -776,8 +776,13 @@ public class FlutterBluePlugin implements FlutterPlugin, ActivityAware, MethodCa
                         if (macDeviceScanned.contains(result.getDevice().getAddress())) return;
                         macDeviceScanned.add(result.getDevice().getAddress());
                     }
-                    Protos.ScanResult scanResult = ProtoMaker.from(result.getDevice(), result);
-                    invokeMethodUIThread("ScanResult", scanResult.toByteArray());
+                    try {
+                        Protos.ScanResult scanResult = ProtoMaker.from(result.getDevice(), result);
+                        invokeMethodUIThread("ScanResult", scanResult.toByteArray());
+                    } catch (Exception exception) {
+                        // exception thrown while parsing the scan result. Ignore the result.
+                        exception.printStackTrace();
+                    }
                 }
 
                 @Override
