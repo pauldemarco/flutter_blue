b037019fd840018459f74e2ae69f49075c518145
Swanav <swanav@outlook.com>
2021-10-20 00:27:06 +0530

    Updates to the Flutter SDK needed these changes

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -54,7 +54,7 @@ class BluetoothOffScreen extends StatelessWidget {
               'Bluetooth Adapter is ${state != null ? state.toString().substring(15) : 'not available'}.',
               style: Theme.of(context)
                   .primaryTextTheme
-                  .subhead
+                  .subtitle1
                   ?.copyWith(color: Colors.white),
             ),
           ],
