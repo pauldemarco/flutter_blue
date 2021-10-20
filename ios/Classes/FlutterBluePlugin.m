d42409222ab810bb89501ba92448a03a49f817ac
Tapana Voravoothiwat <tapana.voravoothiwat@osim.com.sg>
2021-10-20 13:53:11 +0800

    Delay the bluetooth initilization until the app start using it.

--- ios/Classes/FlutterBluePlugin.m
+++ ios/Classes/FlutterBluePlugin.m
@@ -48,7 +48,6 @@ typedef NS_ENUM(NSUInteger, LogLevel) {
   FlutterEventChannel* stateChannel = [FlutterEventChannel eventChannelWithName:NAMESPACE @"/state" binaryMessenger:[registrar messenger]];
   FlutterBluePlugin* instance = [[FlutterBluePlugin alloc] init];
   instance.channel = channel;
-  instance.centralManager = [[CBCentralManager alloc] initWithDelegate:instance queue:nil];
   instance.scannedPeripherals = [NSMutableDictionary new];
   instance.servicesThatNeedDiscovered = [NSMutableArray new];
   instance.characteristicsThatNeedDiscovered = [NSMutableArray new];
@@ -63,6 +62,17 @@ typedef NS_ENUM(NSUInteger, LogLevel) {
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if(self.centralManager == nil){
+    self.centralManager = [[CBCentralManager alloc] initWithDelegate:self queue:nil];
+  }
+    
+  if(self.centralManager.state == CBManagerStateUnknown){
+    // Waiting for the CoreBluetooth to init, try again after 50 ms.
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.05 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [self handleMethodCall:call result:result];
+    });
+  }
+
   if ([@"setLogLevel" isEqualToString:call.method]) {
     NSNumber *logLevelIndex = [call arguments];
     _logLevel = (LogLevel)[logLevelIndex integerValue];
