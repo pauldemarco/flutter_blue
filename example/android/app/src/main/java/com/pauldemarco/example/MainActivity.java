dc7db077e66649130f3e5bd9d95f07a7bc471c6e
chris <hugochou@163.com>
2024-01-22 14:38:39 +0800

    - 适配 Android 12 蓝牙权限（修改 android/src/main/AndroidManifest.xml）

---
+++ example/android/app/src/main/java/com/pauldemarco/example/MainActivity.java
@@ -0,0 +1,6 @@
+package com.pauldemarco.example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- example/android/app/src/main/kotlin/com/pauldemarco/flutter_blue_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.pauldemarco.flutter_blue_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
